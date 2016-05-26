f042da0282de86450a86e0f0403afb1fc98894bc
Zerc <syltaris@gmail.com>
2016-05-27 03:03:25 +0800

    add missing incomplete files for v0.1

--- app/src/main/java/com/kludge/wakemeup/AlarmDetails.java
+++ app/src/main/java/com/kludge/wakemeup/AlarmDetails.java
@@ -4,4 +4,19 @@ package com.kludge.wakemeup;
  * Created by Zex on 26/5/2016.
  */
 public class AlarmDetails {
+    public int nHour;
+    public int nMin;
+    public String strName;
+    public boolean bOnState;
+
+    public AlarmDetails(int nHour, int nMin, String strName){
+        this.nHour = nHour;
+        this.nMin = nMin;
+        this.strName = strName;
+        this.bOnState = false;
+    }
+
+    public void setName(String strName) {this.strName = strName;}
+    public void setTime(int nHour, int nMin) {this.nHour = nHour; this.nMin = nMin;}
+    public void toggleOnState() {bOnState = !bOnState;}
 }
--- app/src/main/java/com/kludge/wakemeup/TimePickerFragment.java
+++ app/src/main/java/com/kludge/wakemeup/TimePickerFragment.java
@@ -1,7 +1,16 @@
 package com.kludge.wakemeup;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.ListView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
 /**
  * Created by Zex on 26/5/2016.
  */
-public class TimePickerFragment {
-}
+
